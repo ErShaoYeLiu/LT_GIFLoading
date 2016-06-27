639eecfcaab5f4003e4e81b253334c7c19445281
wangyuan <wangyuan47156@163.com>
2016-06-27 20:43:45 +0800

    Initial Commit

---
+++ LTGIF_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTGIF_Demo
+//
+//  Created by rimi on 16/6/27.
+//  Copyright © 2016年 liutao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTGIF_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTGIF_Demo
+//
+//  Created by rimi on 16/6/27.
+//  Copyright © 2016年 liutao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
